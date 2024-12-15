b826be200fa5547ea9b42fe739bd8aa65151f9f1
Matt Hova <matthova87@gmail.com>
2024-12-14 19:49:23 -0800

    add signin redirect

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: "/__/auth/:path*",
+        destination:
+          "https://hova-labs-nextjs-firebase-demo.firebaseapp.com/__/auth/:path*",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
